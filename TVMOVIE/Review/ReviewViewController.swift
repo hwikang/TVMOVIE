670eb2935349a19ab4b78a2d026ae5974c74780e
강휘 <kh@paytalab.com>
2024-03-01 14:41:50 +0900

    expandable 컬렉션뷰 구현

--- TVMOVIE/Review/ReviewViewController.swift
+++ TVMOVIE/Review/ReviewViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 import RxSwift
 import Kingfisher
-
 fileprivate enum Section {
     case list
 }
 
+fileprivate enum ListItem: Hashable {
+    case header(ReviewHeader)
+    case content(Review)
+}
+fileprivate struct ReviewHeader: Hashable {
+    let id: String
+    let name: String
+    let url : String
+}
+fileprivate struct Review: Hashable {
+    let content: String
+}
 final class ReviewViewModel {
     
     private let reviewNetwork: ReviewNetwork
     private let contentID: Int
     private let contentType: ContentType
-    //https://api.themoviedb.org/3/movie/933131/reviews?api_key=8d4d29292f768393c47103c8312674f5
     init(contentID: Int, contentType: ContentType) {
         self.contentID = contentID
         self.contentType = contentType
@@ -48,13 +58,14 @@ final class ReviewViewModel {
 final class ReviewViewController: UIViewController {
     private let disposeBag = DisposeBag()
     private let viewModel: ReviewViewModel
-    private var dataSource: UICollectionViewDiffableDataSource<Section, ReviewModel>?
+    private var dataSource: UICollectionViewDiffableDataSource<Section, ListItem>?
     private let collectionView = {
-        let config = UICollectionLayoutListConfiguration(appearance: .grouped)
+        let config = UICollectionLayoutListConfiguration(appearance: .plain)
         let listLayout = UICollectionViewCompositionalLayout.list(using: config)
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: listLayout)
+        collectionView.register(ReviewHeaderCollectionViewCell.self, forCellWithReuseIdentifier: ReviewHeaderCollectionViewCell.id)
         collectionView.register(ReviewCollectionViewCell.self, forCellWithReuseIdentifier: ReviewCollectionViewCell.id)
-
+        
         return collectionView
     }()
     init(contentID: Int, contentType: ContentType) {
@@ -79,6 +90,19 @@ final class ReviewViewController: UIViewController {
             make.edges.equalToSuperview()
         }
         setDataSource()
+        collectionView.rx.itemSelected.bind { [weak self] indexPath in
+            guard let item = self?.dataSource?.itemIdentifier(for: indexPath),
+                    var sectionSnapshot = self?.dataSource?.snapshot(for: .list) else { return }
+
+            if case .header = item {
+                if sectionSnapshot.isExpanded(item) {
+                    sectionSnapshot.collapse([item])
+                } else {
+                    sectionSnapshot.expand([item])
+                }
+            }
+            self?.dataSource?.apply(sectionSnapshot, to: .list)
+        }.disposed(by: disposeBag)
     }
     
     private func bindViewModel() {
@@ -96,44 +120,55 @@ final class ReviewViewController: UIViewController {
             }
             .bind { [weak self] reviewList in
                 guard !reviewList.isEmpty else { return }
-                var snapshot = NSDiffableDataSourceSnapshot<Section, ReviewModel>()
-                snapshot.appendSections([.list])
-                snapshot.appendItems(reviewList, toSection: .list)
-                self?.dataSource?.apply(snapshot, animatingDifferences: false)
+               
+                var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<ListItem>()
+                reviewList.forEach { review in
+                    var name = review.author.name.isEmpty ? review.author.username : review.author.name
+                    let header = ReviewHeader(id: review.id, name: name, url: review.author.imageURL)
+                    let headerListItem = ListItem.header(header)
+                    
+                    sectionSnapshot.append([headerListItem])
+                    sectionSnapshot.append([.content(Review(content: review.content))], to: headerListItem)
+                    
+                }
+                self?.dataSource?.apply(sectionSnapshot, to: .list)
 
             }.disposed(by: disposeBag)
     }
     
     private func setDataSource() {
         //디폴트 셀 사용시 사용이 어려움 -> 패스
-        self.dataSource = UICollectionViewDiffableDataSource<Section, ReviewModel>(collectionView: collectionView) {
-            (collectionView: UICollectionView, indexPath: IndexPath, item: ReviewModel) -> UICollectionViewCell? in
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewCollectionViewCell.id, for: indexPath) as? ReviewCollectionViewCell
-            var text: String
-            if !item.author.name.isEmpty {
-                text = item.author.name
-            } else {
-                text = item.author.username
+        self.dataSource = UICollectionViewDiffableDataSource<Section, ListItem>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, item: ListItem) -> UICollectionViewCell? in
+            switch item {
+            case .header(let header):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewHeaderCollectionViewCell.id, for: indexPath) as? ReviewHeaderCollectionViewCell
+                cell?.configure(title: header.name, url: header.url)
+                return cell
+            case .content(let content):
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ReviewCollectionViewCell.id, for: indexPath) as? ReviewCollectionViewCell
+                cell?.configure(content: content.content)
+                return cell
             }
-            
-            cell?.configure(title: text, url: item.author.imageURL)
-            return cell
         }
-
+        var dataSourceSnapshot = NSDiffableDataSourceSnapshot<Section, ListItem>()
+        dataSourceSnapshot.appendSections([.list])
+        dataSource?.apply(dataSourceSnapshot)
     }
 }
 
-final class ReviewCollectionViewCell: UICollectionViewCell {
-    static let id = "ReviewCollectionViewCell"
+final class ReviewHeaderCollectionViewCell: UICollectionViewCell {
+    static let id = "ReviewHeaderCollectionViewCell"
     private let image: UIImageView = {
         let imageView = UIImageView()
         imageView.layer.masksToBounds = true
         imageView.layer.cornerRadius = 8
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.font = .systemFont(ofSize: 20, weight: .regular)
         return label
     }()
    
@@ -142,20 +177,59 @@ final class ReviewCollectionViewCell: UICollectionViewCell {
         super.init(frame: frame)
         addSubview(image)
         addSubview(titleLabel)
-        
+        snp.makeConstraints { make in
+            make.height.equalTo(44)
+        }
         image.snp.makeConstraints { make in
             make.leading.top.bottom.equalToSuperview()
             make.width.equalToSuperview().multipliedBy(0.3)
         }
         titleLabel.snp.makeConstraints { make in
             make.leading.equalTo(image.snp.trailing).offset(8)
-            make.top.equalToSuperview()
+            make.centerY.equalToSuperview()
         }
     }
     
     public func configure(title: String, url: String) {
         titleLabel.text = title
-        image.kf.setImage(with: URL(string: url))
+        if url.isEmpty {
+            image.image = UIImage(systemName: "person.fill")
+        } else {
+            image.kf.setImage(with: URL(string: url))
+
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+final class ReviewCollectionViewCell: UICollectionViewCell {
+    static let id = "ReviewCollectionViewCell"
+   
+    let contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        label.numberOfLines = 0
+        return label
+    }()
+   
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(contentLabel)
+        
+        contentLabel.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview().inset(20)
+            make.leading.trailing.equalToSuperview().inset(14)
+        }
+    }
+    
+    public func configure(content: String) {
+        contentLabel.text = content
+       
     }
     
     required init?(coder: NSCoder) {
